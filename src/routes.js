144bfced972cddadd808cc320a60b93c1ab64df2
hiagopsilva <hiagohps360@gmail.com>
2020-04-26 00:23:48 -0300

    update validation

--- src/routes.js
+++ src/routes.js
@@ -28,8 +28,18 @@ routes.get('/profile', celebrate({
   }).unknown(),
 }), ProfileController.index);
 
-routes.get('/incidents', IncidentController.index);
+routes.get('/incidents', celebrate({
+  [Segments.QUERY]: Joi.object().keys({
+    page: Joi.number(),
+  })
+}), IncidentController.index);
+
 routes.post('/incidents', IncidentController.create);
-routes.delete('/incidents/:id', IncidentController.delete);
+
+routes.delete('/incidents/:id', celebrate({
+  [Segments.PARAMS]: Joi.object().keys({
+    id: Joi.number().required(),
+  }),
+}), IncidentController.delete);
 
 module.exports = routes;
