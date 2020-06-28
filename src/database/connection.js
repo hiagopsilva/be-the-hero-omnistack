5ced105acf44ab0dd32725419ce08bd91cffa9f1
hiagopsilva <hiagohps360@gmail.com>
2020-06-28 01:56:11 -0300

    .

--- src/database/connection.js
+++
@@ -1,8 +0,0 @@
-const knex = require('knex');
-const configutarion = require('../../knexfile');
-
-const config = process.env.NODE_ENV == 'test' ? configutarion.test : configutarion.development;
-
-const connection = knex(config);
-
-module.exports = connection;
